9774bf9fbbb10fae9c76f3e9be57d5f6badb1662
Gokayy1 <gokayyener713@hotmail.com>
2025-01-13 20:11:42 +0300

    a

--- php/login.php
+++ php/login.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="tr">
 <head>
@@ -6,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login</title>
-
     
     <link rel="stylesheet" href="../css/base.css" />
     <link rel="stylesheet" href="../css/buttons.css" />
@@ -15,57 +13,59 @@
 </head>
 <body id="body">
     <header class="main-header">
-      <a href="Homepage.html" class="main-header__logo">Vegetable</a>
-      <nav class="main-nav">
-        <ul class="main-nav__items">
-          <li class="main-nav__item">
-            <a href="Homepage.html">Homepage</a>
-          </li>
-        </ul>
-      </nav>
+        <a href="Homepage.html" class="main-header__logo">Vegetable</a>
+        <nav class="main-nav">
+            <ul class="main-nav__items">
+                <li class="main-nav__item">
+                    <a href="Homepage.html">Homepage</a>
+                </li>
+            </ul>
+        </nav>
     </header>
     <main class="signup-page">
-      <h1 class="signup-title">Login Your Account</h1>
-      <form action="login_process.php" method="POST" class="signup-form" id="signup-form">
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" required />
+        <h1 class="signup-title">Login Your Account</h1>
 
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" required />
+        <?php
+        if (isset($_GET['error'])) {
+            echo "<p style='color: red;'>" . htmlspecialchars($_GET['error']) . "</p>";
+        }
+        ?>
 
-        <button type="submit" class="btn btn-primary login__button">
-          Login
-        </button>
-      </form>
+        <form action="php/login_process.php" method="POST" class="signup-form" id="signup-form">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" required />
+
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required />
+
+            <button type="submit" class="btn btn-primary login__button">Login</button>
+        </form>
     </main>
 
     <footer class="footer_home">
-      <div class="footer_home-container">
-        <div class="footer_home-section">
-          <h4>About Us</h4>
-          <p>
-            We are dedicated to providing the best services and products to our
-            customers. Learn more about us and our mission.
-          </p>
-        </div>
+        <div class="footer_home-container">
+            <div class="footer_home-section">
+                <h4>About Us</h4>
+                <p>We are dedicated to providing the best services and products to our customers.</p>
+            </div>
 
-        <div class="footer_home-section">
-          <h4>Contact</h4>
-          <a href="about.html">Contact/About</a>
-        </div>
+            <div class="footer_home-section">
+                <h4>Contact</h4>
+                <a href="about.html">Contact/About</a>
+            </div>
 
-        <div class="footer_home-section">
-          <h4>Follow Us</h4>
-          <a href="https://facebook.com" target="_blank">Facebook</a>
-          <a href="https://twitter.com" target="_blank">Twitter</a>
-          <a href="https://instagram.com" target="_blank">Instagram</a>
-          <a href="https://linkedin.com" target="_blank">LinkedIn</a>
-        </div>
+            <div class="footer_home-section">
+                <h4>Follow Us</h4>
+                <a href="https://facebook.com" target="_blank">Facebook</a>
+                <a href="https://twitter.com" target="_blank">Twitter</a>
+                <a href="https://instagram.com" target="_blank">Instagram</a>
+                <a href="https://linkedin.com" target="_blank">LinkedIn</a>
+            </div>
 
-        <div class="footer_home-bottom">
-          <p>&copy; 2025 Your Company Name. All rights reserved.</p>
+            <div class="footer_home-bottom">
+                <p>&copy; 2025 Your Company Name. All rights reserved.</p>
+            </div>
         </div>
-      </div>
     </footer>
-  </body>
+</body>
 </html>
--- php/login_process.php
+++ php/login_process.php
@@ -9,29 +9,29 @@ $dbname = "vegetable_db1";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else 
- echo("Connection Succaaaa"); 
-
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $password = $_POST["password"];
 
-    
     $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-       
+        
         $_SESSION["user"] = $email;
-        header("Location: Homepage.html"); 
+
+       
+        header("Location: ../Homepage.html");
         exit();
     } else {
-        echo "Invalid email or password.";
+        
+        $error_message = "Invalid email or password.";
+        header("Location: ../login.php?error=" . urlencode($error_message));
+        exit();
     }
 }
 
